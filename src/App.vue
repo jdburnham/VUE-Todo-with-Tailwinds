a4706d07b149c7c9494e4fd5665de7758760f624
jdburnham <jburnham@stu.southtexascollege.edu>
2020-01-28 15:18:48 -0600

    Finish Todo App w/ VUE

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,100 @@
 <template>
-  <h1>Hello There!</h1>
+  <div class="h-screen w-width bg-orange-400 flex flex-col justify-center items-center">
+    <div class="header">        
+        <h1 class="text-4xl">JB Todo App w/ Tailwind & VUE</h1>
+        <div class="form flex">
+            <input 
+            v-model="newTodo"
+            @keyup="checkAdd"
+            class="appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" 
+            id="newText" 
+            type="text" 
+            placeholder="Add New Todo"
+            />
+            <button 
+            id="newBtn"
+            @click="add"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">
+                Add
+            </button>
+        </div>
+    </div>
+    <div class="todos w-1/2 mt-8">
+        <div class="pending" v-if="totalPendings>0">
+            <h3 class="text-2xl">Pending: {{totalPendings}} tasks</h3>
+            <ul id="pendingList">
+                <li
+                v-for="(pending, index) in pendingList"
+                :key="index"
+                @click="moveToCompleted(pending, index)" 
+                class="text-center p-3 text-red-500 bg-red-200 mt-4 rounded shadow-lg cursor-pointer hover:bg-red-700 hover:text-white"
+                >{{pending}}</li>
+            </ul>
+        </div>
+        <div class="completed mt-8" v-if="totalCompleted>0">
+            <h3 class="text-2xl">Completed: {{totalCompleted}} tasks</h3>
+            <ul id="completedList">
+                <li 
+                v-for="(completed, index) in completedList"
+                :key="index" 
+                @click="moveToPending(completed, index)"
+                class="text-center p-3 line-through text-green-500 bg-green-200 mt-4 rounded shadow-lg cursor-pointer hover:bg-green-700 hover:text-white"
+                >{{completed}}</li>
+            </ul>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+export default 
+{
   name: "app",
+  data() {
+    return{
+      newTodo:"",
+      pendingList:[],
+      completedList:[]
+    }
+  },
+  computed:
+  {
+    totalPendings(){
+      return this.pendingList.length;
+    },
+    totalCompleted(){
+      return this.completedList.length;
+    }
+  },
+  methods:
+  {
+    add() 
+    {
+      if (this.newTodo!="") {
+        this.pendingList.push(this.newTodo)
+        this.newTodo="";
+      } else {
+        alert("Please input a new task.");
+      }
+    },
+    checkAdd(event)
+    {
+      if(event.keyCode === 13){
+        event.preventDefault();
+        this.add();
+      }
+    },
+    moveToCompleted(todo, index)
+    {
+      this.completedList.push(todo);
+      this.pendingList.splice(index, 1);
+    },
+    moveToPending(todo, index)
+    {
+      this.pendingList.push(todo);
+      this.completedList.splice(index, 1);
+    }
+  }
 };
 </script>
 
